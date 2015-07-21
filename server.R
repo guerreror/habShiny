3224e9bb93dda88dbb8dfa2619036bcf4924e58b
Rafael Guerrero <rafguerr@indiana.edu>
2015-07-21 14:07:18 -0400

    Initial

---
+++ server.R
@@ -0,0 +1,75 @@
+
+# This is the server logic for a Shiny web application.
+#
+## To deploy, use:
+#library(shinyapps)
+#shinyapps::setAccountInfo(name='hawkmoth', ...
+#shinyapps::deployApp('~/Box Sync/habDX/shinyHab/', appName ="habDX")
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+
+shinyServer(function(input, output) {
+  
+  geneNames <-read.table("MasterList_GeneNamesTable.txt", header=T, colClasses=c("character", "character"))
+  geneNames<- mutate(geneNames, type= substr(Name,1,1))
+  
+  val <- reactiveValues()
+  
+  observeEvent(label = "newData",
+               eventExpr = input$dataset,
+               handlerExpr ={
+                 withProgress(message = 'Loading data...',
+                              value = 0, {for (i in 1:10) {incProgress(1/10)} })
+                 val$fileName <- as.character(input$dataset);
+                 val$currentData <- read.table(val$fileName, header=T)
+               }
+  )
+  
+  datReact <- reactive({   
+    val$currentData %>%
+      filter( !input$onlyITAG | geneNames$type[match(Gene, geneNames$Name)] == "S" ) %>% 
+      filter(P.Value < 10^(-input$pval) ) %>% filter(input$includeDown | logFC > 0) %>% 
+      mutate( logThres = factor(input$logFC < abs(logFC) )) 
+  } )
+  
+  geneTable <-reactive({
+    datReact()%>%
+      filter(logThres == TRUE ) %>% 
+      arrange(desc(abs(logFC) )) %>% 
+      transmute(logFC = logFC, 
+                AvgExpr = AveExpr, 
+                p = P.Value, 
+                Gene = Gene, 
+                Note = geneNames$Note[match(Gene, geneNames$Name)])
+  })
+  
+  output$histPlot <- renderPlot({
+    ggplot(datReact()) + geom_bar(binwidth = 0.5, aes(x = logFC, fill = logThres )) +
+      scale_fill_manual(values=c("grey90", "grey50"))+
+      theme_classic()+theme(legend.position = "none")+
+      geom_vline(xintercept = c(input$logFC, -input$logFC*as.numeric(input$includeDown)), colour= c("green", "red"))+
+      labs(x="Expression difference A-B (log2 fold change)") 
+  })
+  
+  output$selectedNumber <- renderText({
+    selected <- sum( datReact()$logThres ==TRUE )
+    total <- length(val$currentData[,1])
+    sprintf("Selected %i genes (%.1f%% of total)", selected, 100*selected/total)
+  })
+  
+  output$topTable <- renderTable({
+    tt<-head(geneTable(), n=20)
+    tt$p<-prettyNum(tt$p, format='e', digits=3)
+    tt
+  })
+  
+  output$downloadGeneTable <- downloadHandler(filename =
+                                                function() {paste('Hab_genes_', Sys.Date(), '.txt', sep='')}, 
+                                              content = function(file) {write.table(geneTable(), file, row.names=F)}
+  )
+  
+  
+}) #end shinyServer fn
---
+++ ui.R
@@ -0,0 +1,64 @@
+
+# This is the user-interface definition of a Shiny web application.
+
+library(shiny)
+
+dataset_names <- list(
+  "Pollen (A) vs. Style (B)"= as.character("hab_pollen_vs_style_table.txt"),
+  "Pollen IP (A) vs. no IP (B)" = as.character("hab_pollenIP_table.txt"),
+  "Style UI (A) vs. no UI (B)" = as.character("hab_styleUI_table.txt"),
+  "Style IP (A) vs. no IP (B)"= as.character("hab_styleIP_table.txt")
+)
+
+fluidPage(tabsetPanel(
+  tabPanel("Lists of genes",
+           pageWithSidebar(
+             # Application title
+             headerPanel("S.habrochaites differential gene expression"),
+             
+             # Sidebar with input
+             sidebarPanel(
+               # dataset drop-down
+               selectInput("dataset", "Choose a contrast:", dataset_names),
+               
+               # pVal bar
+               sliderInput("pval", "P-value threshold (-log10):", min = 2, max = 8, value = 3),
+               
+               # fold-change bar
+               sliderInput("logFC", "Fold-change (log2):", min = 1, max = 15, value = 1),
+               
+               # checkbox for Lyco genes only
+               checkboxInput("onlyITAG", "ITAG genes only (genes from S. lycopersicum)", TRUE),
+               
+               # include down-reg genes
+               checkboxInput("includeDown", "Include down-regulated genes", FALSE),
+
+               # download button
+               downloadButton('downloadGeneTable', 'Download table of selected genes'),
+               
+               br(),
+               br(),
+               h4("Description of contrasts"),
+               p(em("Pollen vs. Style:"),
+                 "Genes different between tissues, across all hab accessions."),
+               p(em("Pollen-side IP:"), 
+                 "Genes different in pollen of accessions [1777,2119,1264] vs. [2098,407,1223], excludes genes that are also different in styles."),
+               p(em("Style-side UI:"),
+                 "Genes different in styles of accessions [1777,2098,2119] vs. [407,1223], excludes genes that are also different in pollen."),
+               p(em("Style-side IP:"),
+                 "Genes different in styles of accessions [1777, 2098] vs. [2119,1223], excludes genes that are also different in pollen.")
+             ),
+             
+             #main for output
+             mainPanel(
+               plotOutput("histPlot"),
+               h3(textOutput("selectedNumber")),
+               p("Here is the Top-20 (sorted by log2[fold change]):"),
+               tableOutput("topTable")
+             )
+           )
+  )
+  
+
+) #end Tabset Panel
+) #end fluid page
